4bc955fae2e23d395ea0f42e786605c8c7b52fb4
verastian <verastian0908@gmail.com>
2023-11-08 23:43:22 -0300

    feature:inventory-system

---
+++ www/html/inventory_system/includes/config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: OSWA-INV
+| Version: v2
+| Offcial page: http://oswapp.com/
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+define('DB_HOST', 'db'); // Set database host
+define('DB_USER', 'root'); // Set database user
+define('DB_PASS', 'test'); // Set database password
+define('DB_NAME', 'oswa_inv'); // Set database name
+
+?>
